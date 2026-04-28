e530393998fa5be542ea49164c7fc1682a28d0ef
niobed <niiobed13@gmail.com>
2026-04-28 09:59:22 +0100

    cookies edits

--- jaysbite/src/assets/data/snacks.js
+++ jaysbite/src/assets/data/snacks.js
@@ -1,5 +1,23 @@
 const snacks = [
   {
+    name: "Chocolate Piped Cookies",
+    image: "/images/products/Chocolatepiped.jpeg",
+    price: "GH₵ 40",
+    details: "Available in stock",
+  },
+  {
+    name: "Cinnamon Piped Cookies",
+    image: "/images/products/Cinnamonpiped.jpeg",
+    price: "GH₵ 40",
+    details: "Available in stock",
+  },
+  {
+    name: "Vanilla Piped Cookies",
+    image: "/images/products/Vanillapiped.jpeg",
+    price: "GH₵ 40",
+    details: "Available in stock",
+  },
+  {
     name: "CAKE PARFAIT",
     image: "/images/products/cakeparfait.webp",
     price: "GH₵ 60",
@@ -21,30 +39,6 @@ const snacks = [
     name: "CAKE LOAF",
     image: "/images/products/cakeloaf.webp",
     price: "GH₵ 60",
-    details: "Available in stock",
-  },
-  {
-    name: "Cookies",
-    image: "/images/products/cookie.webp",
-    price: "GH₵ 40",
-    details: "Available in stock",
-  },
-  {
-    name: "Chocolate Piped Cookies",
-    image: "/images/products/Chocolatepiped.jpeg",
-    price: "GH₵ 40",
-    details: "Available in stock",
-  },
-  {
-    name: "Cinnamon Piped Cookies",
-    image: "/images/products/Cinnamonpiped.jpeg",
-    price: "GH₵ 40",
-    details: "Available in stock",
-  },
-  {
-    name: "Vanilla Piped Cookies",
-    image: "/images/products/Vanillapiped.jpeg",
-    price: "GH₵ 40",
     details: "Available in stock",
   },
 
